13abe3c798182ea2c9fb8212c2f1beacc276c099
FantomGSS <gogo.stanchev@gmail.com>
2022-05-23 10:48:33 +0300

    Shapes section

--- Paint/ribbon/ribbon.js
+++ Paint/ribbon/ribbon.js
@@ -208,6 +208,14 @@ function dropdownBrushes() {
 	document.getElementById("brushesDropdown").classList.toggle("show");
 }
 
+function dropdownOutline() {
+	document.getElementById("outlineDropdown").classList.toggle("show");
+}
+
+function dropdownFill() {
+	document.getElementById("fillDropdown").classList.toggle("show");
+}
+
 window.onclick = function(event) {
 	if (!event.target.matches('.button-title')) {
 		var dropdowns = document.getElementsByClassName("dropdown-content");
@@ -219,4 +227,23 @@ window.onclick = function(event) {
 			}
 		}
 	}
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+	showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+	let i;
+	let slides = document.getElementsByClassName("slide");
+	if (n > slides.length) {slideIndex = 1}
+	if (n < 1) {slideIndex = slides.length}
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+
+	slides[slideIndex-1].style.display = "block";
 }
