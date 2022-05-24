898717f8e1d09c6b9c10966fd8ec5e0bf77f235e
FantomGSS <gogo.stanchev@gmail.com>
2022-05-24 17:09:18 +0300

    Line sizes and colors section

--- Paint/ribbon/ribbon.js
+++ Paint/ribbon/ribbon.js
@@ -9,9 +9,7 @@
 		var that = function() { 
 			return thatRet;
 		};
-		
-		
-		
+
 		var thatRet = that;
 		
 		that.selectedTabIndex = -1;
@@ -21,10 +19,11 @@
 		that.goToBackstage = function() {
 			ribObj.addClass('backstage');
 		}
-			
+
 		that.returnFromBackstage = function() {
 			ribObj.removeClass('backstage');
-		}	
+		}
+
 		var ribObj = null;
 		
 		that.init = function(id) {
@@ -32,7 +31,7 @@
 				id = 'ribbon';
 			}
 		
-			ribObj = $('#'+id);
+			ribObj = $('#' + id);
 			ribObj.find('.ribbon-window-title').after('<div id="ribbon-tab-header-strip"></div>');
 			var header = ribObj.find('#ribbon-tab-header-strip');
 			
@@ -40,15 +39,16 @@
 				var id = $(this).attr('id');
 				if (id === undefined || id === null)
 				{
-					$(this).attr('id', 'tab-'+index);
-					id = 'tab-'+index;
+					$(this).attr('id', 'tab-' + index);
+					id = 'tab-' + index;
 				}
 				tabNames[index] = id;
 			
 				var title = $(this).find('.ribbon-title');
 				var isBackstage = $(this).hasClass('file');
-				header.append('<div id="ribbon-tab-header-'+index+'" class="ribbon-tab-header"></div>');
-				var thisTabHeader = header.find('#ribbon-tab-header-'+index);
+				header.append('<div id="ribbon-tab-header-' + index + '" class="ribbon-tab-header"></div>');
+
+				var thisTabHeader = header.find('#ribbon-tab-header-' + index);
 				thisTabHeader.append(title);
 				if (isBackstage) {
 					thisTabHeader.addClass('file');
@@ -68,9 +68,7 @@
 						that.switchToTabByIndex(index);
 					});
 				}
-				
-				
-				
+
 				$(this).hide();
 			});
 			
@@ -78,27 +76,11 @@
 				var title = $(this).find('.button-title');
 				title.detach();
 				$(this).append(title);
-				
-				var el = $(this);
-				
-				this.enable = function() {
-					el.removeClass('disabled');
-				}
-				this.disable = function() {
-					el.addClass('disabled');
-				}
-				this.isEnabled = function() {
-					return !el.hasClass('disabled');
-				}
-								
+
 				if ($(this).find('.ribbon-hot').length === 0) {
 					$(this).find('.ribbon-normal').addClass('ribbon-hot');
-				}			
-				if ($(this).find('.ribbon-disabled').length === 0) {
-					$(this).find('.ribbon-normal').addClass('ribbon-disabled');
-					$(this).find('.ribbon-normal').addClass('ribbon-implicit-disabled');
 				}
-				
+
 				$(this).tooltip({
 					bodyHandler: function () {
 						if (!$(this).isEnabled()) { 
@@ -127,55 +109,18 @@
 				});
 			});
 
-			// ribObj.find('.ribbon-section').each(function(index) {
-			// 	$(this).after('<div class="ribbon-section-sep"></div>');
-			// });
-
-			ribObj.find('div').attr('unselectable', 'on');
-			ribObj.find('span').attr('unselectable', 'on');
-			ribObj.attr('unselectable', 'on');
-
 			that.switchToTabByIndex(that.selectedTabIndex);
 		}
 		
 		that.switchToTabByIndex = function(index) {
 			var headerStrip = $('#ribbon #ribbon-tab-header-strip');
 			headerStrip.find('.ribbon-tab-header').removeClass('sel');
-			headerStrip.find('#ribbon-tab-header-'+index).addClass('sel');
+			headerStrip.find('#ribbon-tab-header-' + index).addClass('sel');
 
 			$('#ribbon .ribbon-tab').hide();
-			$('#ribbon #'+tabNames[index]).show();
+			$('#ribbon #' + tabNames[index]).show();
 		}
-		
-		$.fn.enable = function() {
-			if (this.hasClass('ribbon-button')) {
-				if (this[0] && this[0].enable) {
-					this[0].enable();
-				}	
-			}
-			else {
-				this.find('.ribbon-button').each(function() {
-					$(this).enable();
-				});
-			}				
-		}
-		
-		
-			
-				
-		$.fn.disable = function() {
-			if (this.hasClass('ribbon-button')) {
-				if (this[0] && this[0].disable) {
-					this[0].disable();
-				}	
-			}
-			else {
-				this.find('.ribbon-button').each(function() {
-					$(this).disable();
-				});
-			}				
-		}
-	
+
 		$.fn.isEnabled = function() {
 			if (this[0] && this[0].isEnabled) {
 				return this[0].isEnabled();
@@ -183,10 +128,8 @@
 				return true;
 			}
 		}
-	
-	
+
 		that.init(id);
-	
 		$.fn.ribbon = that;
 	};
 
@@ -214,6 +157,10 @@ function dropdownOutline() {
 
 function dropdownFill() {
 	document.getElementById("fillDropdown").classList.toggle("show");
+}
+
+function dropdownSize() {
+	document.getElementById("sizeDropdown").classList.toggle("show");
 }
 
 window.onclick = function(event) {
